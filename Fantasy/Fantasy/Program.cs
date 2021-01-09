574df2847829e806075f15ef79ebe62b5c48aeb3
KarimAmrM <59086314+KarimAmrM@users.noreply.github.com>
2021-01-09 19:14:14 +0200

    Open on form1

--- Fantasy/Fantasy/Program.cs
+++ Fantasy/Fantasy/Program.cs
@@ -17,7 +17,7 @@ namespace Fantasy
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new AddFixture());
+            Application.Run(new Form1());
 
         }
     }
